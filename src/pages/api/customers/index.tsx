b118d02869289d66195dd6ea204a1ee7fcbeb32d
emmanueluwa <emmanuel@fulodev.com>
2024-01-06 14:18:29 +0000

    adding ability to post new document to mongo db using nextapi

--- src/pages/api/customers/index.tsx
+++ src/pages/api/customers/index.tsx
@@ -5,6 +5,7 @@ ENDPOINT: GET ALL CUSTOMERS && ADD NEW CUSTOMERS
 import type { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "../../../../lib/mongodb";
 import { Customer } from "@/pages/customers";
+import { ObjectId } from "mongodb";
 
 type ReturnCustomers = {
   customers: Customer[];
@@ -22,12 +23,45 @@ export const getCustomers = async (): Promise<Customer[]> => {
   return JSON.parse(JSON.stringify(data));
 };
 
+//POST: Adding new customer
+export const addCustomer = async (customer: Customer): Promise<ObjectId> => {
+  const mongoClient = await clientPromise;
+
+  const response = await mongoClient
+    .db()
+    .collection("customers")
+    .insertOne(customer);
+
+  //user can use the id to get the customer
+  return response.insertedId;
+};
+
 const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<ReturnCustomers>
+  res: NextApiResponse<ReturnCustomers | ObjectId | { error: string }>
 ) => {
-  const data = await getCustomers();
-  res.status(200).json({ customers: data });
+  //
+  if (req.method === "GET") {
+    const data = await getCustomers();
+
+    res.status(200).json({ customers: data });
+  } else if (req.method === "POST") {
+    //expecting customer to be sent with request
+    console.log(req.body);
+
+    if (req.body.name && req.body.industry) {
+      //validation to avoid bogus data database. FYI: frontend forms html/css/js can be bypassed
+      const customer: Customer = {
+        name: req.body.name,
+        industry: req.body.industry,
+      };
+
+      const insertedId = await addCustomer(customer);
+      res.status(200).json(insertedId);
+    } else {
+      res.status(400).json({ error: "name and industry are requirements" });
+    }
+  }
 };
 
 export default handler;
--- src/pages/customers/index.tsx
+++ src/pages/customers/index.tsx
@@ -5,7 +5,7 @@ import clientPromise from "../../../lib/mongodb";
 import { getCustomers } from "../api/customers";
 
 export type Customer = {
-  _id: ObjectId;
+  _id?: ObjectId;
   name: string;
   industry: string;
 };
@@ -41,8 +41,8 @@ const Customers: NextPage = ({
       <h1>Customers</h1>
       {customers.map((customer: Customer) => {
         return (
-          <div key={customer._id.toString()}>
-            <p>{customer._id.toString()}</p>
+          <div key={customer._id?.toString()}>
+            <p>{customer._id?.toString()}</p>
 
             <p>{customer.name}</p>
             <p>{customer.industry}</p>
