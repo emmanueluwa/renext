81d1ab9d6c0d62bf77e0678d748c23631acbd661
emmanueluwa <emmanuel@fulodev.com>
2024-01-06 07:46:34 +0000

    connecting mongodb and adding the ability to get data from db

--- src/pages/customers/[id].tsx
+++ src/pages/customers/[id].tsx
@@ -1,11 +1,11 @@
 import { NextPage, GetStaticProps, GetStaticPaths } from "next";
 import { useRouter } from "next/router";
 import { Customer } from ".";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { ParsedUrlQuery } from "querystring";
 
 type Props = {
-  customer: Customer;
+  customer?: Customer;
 };
 
 interface Params extends ParsedUrlQuery {
@@ -14,15 +14,15 @@ interface Params extends ParsedUrlQuery {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   //request data
-  const result = await axios.get("http://127.0.0.1:8000/api/customers");
+  // const result = await axios.get("http://127.0.0.1:8000/api/customers");
 
-  const paths = result.data.customers.map((customer: Customer) => {
-    return { params: { id: customer.id.toString() } };
-  });
+  // const paths = result.data.customers.map((customer: Customer) => {
+  //   return { params: { id: customer.id.toString() } };
+  // });
 
   return {
     //all paths to be generated on build
-    paths: paths,
+    paths: [],
     fallback: "blocking",
   };
 };
@@ -32,19 +32,43 @@ export const getStaticProps: GetStaticProps<Props, Params> = async (
 ) => {
   const params = context.params;
 
-  const result = await axios.get<{ customer: Customer }>(
-    `http://127.0.0.1:8000/api/customers/${params?.id}/`
-  );
-  console.log("RES$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$:", result);
-  return {
-    props: {
-      customer: result.data.customer,
-    },
-  };
+  // check if data found
+  try {
+    const result = await axios.get<{ customer: Customer }>(
+      `http://127.0.0.1:8000/api/customers/${params?.id}/`
+    );
+
+    return {
+      props: {
+        customer: result.data.customer,
+      },
+      //checking for changes in db
+      revalidate: 60,
+    };
+  } catch (err) {
+    //TODO: LOOK INTO BUILDING OUT MORE ERROR CASES?
+    if (err instanceof AxiosError) {
+      if (err.response?.status === 404) {
+        return {
+          notFound: true,
+          revalidate: 60,
+        };
+      }
+    }
+    return {
+      props: {},
+    };
+  }
 };
 
 const Customer: NextPage<Props> = (props) => {
-  return <h1>{props.customer.name}</h1>;
+  //check if data is loaded yet
+  const router = useRouter();
+  if (router.isFallback) {
+    return <p>Loading...</p>;
+  }
+
+  return <h1>{props.customer ? props.customer.name : null}</h1>;
 };
 
 export default Customer;
--- src/pages/customers/index.tsx
+++ src/pages/customers/index.tsx
@@ -1,8 +1,9 @@
 import { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
 import axios from "axios";
+import { MongoClient, ObjectId } from "mongodb";
 
 export type Customer = {
-  id: number;
+  _id: ObjectId;
   name: string;
   industry: string;
 };
@@ -12,14 +13,21 @@ type GetCustomerResponse = {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const result = await axios.get<GetCustomerResponse>(
-    "http://127.0.0.1:8000/api/customers/"
-  );
-  console.log(result);
+  // const mongoClient = new MongoClient(process.env.MONGODB!);
+  const mongoClient = new MongoClient(process.env.MONGODB!);
+
+  const data = await mongoClient.db().collection("Customers").find().toArray();
+
+  console.log("$$$$$$$$$$$$$$$$$$$$$!!!!!!!!!!!!!!", data);
+
+  // const result = await axios.get<GetCustomerResponse>(
+  //   "http://127.0.0.1:8000/api/customers/"
+  // );
+  // console.log(result);
 
   return {
     props: {
-      customers: result.data.customers,
+      customers: JSON.parse(JSON.stringify(data)),
     },
     revalidate: 60,
   };
@@ -34,8 +42,8 @@ const Customers: NextPage = ({
       <h1>Customers</h1>
       {customers.map((customer: Customer) => {
         return (
-          <div key={customer.id}>
-            <p>{customer.id}</p>
+          <div key={customer._id.toString()}>
+            <p>{customer._id.toString()}</p>
 
             <p>{customer.name}</p>
             <p>{customer.industry}</p>
